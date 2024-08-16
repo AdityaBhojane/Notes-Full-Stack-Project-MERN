101a42cdbf84d26cca37fd6c9dda4d4260c919e8
aditya bhojane <adityabhojane66@gmail.com>
2024-08-16 21:59:28 +0530

    add default route

--- Frontend/Notes App MERN/vite.config.js
+++ Frontend/Notes App MERN/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // it will set this route as default route
+  base: '/dashboard/',
 })
+
+  
