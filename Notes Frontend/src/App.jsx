c1b873150da226f87f2b93206246d58901701693
aditya bhojane <adityabhojane66@gmail.com>
2024-09-17 02:03:36 +0530

    Add Frontend error fix

--- Notes Frontend/src/App.jsx
+++ Notes Frontend/src/App.jsx
@@ -1,26 +1,22 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import LogIn from "./pages/login/LogIn";
 import SignUp from "./pages/SignUp/SignUp";
-import Modal from 'react-modal';
-
+import Modal from "react-modal";
 // Set the app element to the root element of your app
-// Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 function App() {
-  // Dynamically set the base path depending on the environment
-  // const basePath = import.meta.env.VITE_NODE_ENV === 'production' ? '/login' : '/';
-
+  //  console.log(import.meta.env.VITE_BASE_API_URL)
   return (
-    // <Router basename={basePath}>
-    <Router>
+    <>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/dashboard" element={<Home />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Home />}></Route>
+        <Route path="/login" element={<LogIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
-    </Router>
+    </>
   );
 }
 
